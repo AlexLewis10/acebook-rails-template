78eba8cdfb19e965598f8faff777c0fdc8cb6816
Graham <grahamalconer@icloud.com>
2020-05-11 11:35:27 +0100

    added require uri to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
-
-require 'rails/all'
+require 'uri' #added this to pass require URI travis error
+require 'rails/all' 
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
