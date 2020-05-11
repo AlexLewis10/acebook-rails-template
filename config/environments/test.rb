55ecb1708fe6e163ea5ee3331a42d25cac5597d0
Graham <grahamalconer@icloud.com>
2020-05-11 11:44:25 +0100

    added rails db:migrate to travis yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
   config.cache_classes = true
+  
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
@@ -36,7 +37,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
+  config.active_record.migration_error = true # added to pass travis
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
