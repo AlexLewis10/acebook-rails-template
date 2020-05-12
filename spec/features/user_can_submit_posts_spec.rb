eae2e91475563f4a5be3d5a6516bf827ed91516d
Alex Lewis <alexlewis374@gmail.com>
2020-05-12 17:22:11 +0100

    add rubocop to travis

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Timeline", type: :feature do
+  #test may need to change
   scenario "Can submit posts and view them" do
     User.create(name: "Gina", password: "123456", email: "gina@example.com")
     visit "/sessions/new"
@@ -12,4 +13,17 @@ RSpec.feature "Timeline", type: :feature do
     click_button "Submit"
     expect(page).to have_content("Hello, world!")
   end
+
+  scenario "User can make a new post from their own wall" do
+    User.create(name: "Gina", password: "123456", email: "gina@example.com")
+    visit "/sessions/new"
+    fill_in "session[name]", with: "Gina"
+    fill_in "session[password]", with: "123456"
+    click_on "Log in"
+    click_link "New post"
+    fill_in "Message", with: "Hello, world!"
+    click_button "Submit"
+    expect(page).to have_content("Hello, world!")
+  end
+
 end
