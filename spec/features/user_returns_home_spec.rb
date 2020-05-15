9f64f0e7a7a47fba7da52ac5d53bd5605a1db56c
Graham <grahamalconer@icloud.com>
2020-05-15 14:34:32 +0100

    Tests passing again

--- spec/features/user_returns_home_spec.rb
+++ spec/features/user_returns_home_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature 'user can return home', type: :feature do
     User.create(name: "Gina", password: "123456", email: "gina@example.com")
     log_in_gina
     click_on "Home"
-    expect(page).to have_content('GAPAGbook')
+    expect(page).to have_content('Homepage')
   end
 end
 
